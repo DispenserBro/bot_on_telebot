727596b5cb165fc86cf1163b32dd847b279bd6f6
Danil Iarosh <maze3290@gmail.com>
2022-11-16 05:55:54 +0300

    added menu test
    * menu is currently WIP and may not work as expected, it will be tested
    * this example works as I expected)))

--- bot_app/starters/register_commands.py
+++ bot_app/starters/register_commands.py
@@ -1,5 +1,6 @@
 from handlers.utils.weather import send_weather_image, send_w2_info
-from handlers.messages.default import send_help, echo_message
+from handlers.messages.private import send_help, send_menu  # , echo_message
+from handlers.callback_handlers.example import send_example
 from starters.get_commands_dict import commands_dict as ALL_COMMANDS
 
 # print(ALL_COMMANDS)
@@ -23,7 +24,17 @@ def register_handlers(bot):
     )
 
     bot.register_message_handler(
-        echo_message,
-        func=lambda message: True
+        send_menu,
+        commands=ALL_COMMANDS['menu']['commands'],
+        chat_types=['private']
     )
+
+    bot.register_callback_query_handler(
+        send_example,
+        lambda c: c.data.startswith('menu')
+    )
+    # bot.register_message_handler(
+    #     echo_message,
+    #     func=lambda message: True
+    # )
     return bot
