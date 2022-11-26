1b91a72bbb17730bdff4aa7a58f6cfe38718bffd
Danil Iarosh <maze3290@gmail.com>
2022-11-26 22:39:46 +0300

    minor changes

--- bot_app/starters/register_commands.py
+++ bot_app/starters/register_commands.py
@@ -1,3 +1,5 @@
+from telebot import TeleBot
+
 from handlers.messages import private, groups, admin
 from handlers.utils import weather
 from handlers.callback_handlers import commands_callback
@@ -6,7 +8,7 @@ from starters.get_commands_dict import commands_dict as ALL_COMMANDS
 from starters.register_bot import logger  # , bot
 
 
-def registrer_admin_msg_handlers(bot):
+def registrer_admin_msg_handlers(bot: TeleBot) -> None:
     bot.register_message_handler(
         admin.admin_mute_member,
         func=lambda m: m.text and m.text.startswith('!ro'),
@@ -28,7 +30,7 @@ def registrer_admin_msg_handlers(bot):
     logger.info('admin_msg_handlers registered')
 
 
-def register_private_msg_handlers(bot):
+def register_private_msg_handlers(bot: TeleBot) -> None:
     bot.register_message_handler(
         private.send_help,
         commands=ALL_COMMANDS['help']['commands'],
@@ -38,7 +40,7 @@ def register_private_msg_handlers(bot):
     logger.info('private_msg_handlers registered')
 
 
-def register_group_msg_handlers(bot):
+def register_group_msg_handlers(bot: TeleBot) -> None:
     bot.register_message_handler(
         groups.answer_a,
         regexp=r'.*[Аа]\.',
@@ -48,7 +50,7 @@ def register_group_msg_handlers(bot):
     logger.info('group_msg_handlers registered')
 
 
-def register_utils_handlers(bot):
+def register_utils_handlers(bot: TeleBot) -> None:
     bot.register_message_handler(
         weather.send_weather_image,
         commands=ALL_COMMANDS['weather1']['commands']
@@ -62,7 +64,7 @@ def register_utils_handlers(bot):
     logger.info('utils_handlers registered')
 
 
-def register_callback_query_handlers(bot):
+def register_callback_query_handlers(bot: TeleBot) -> None:
     bot.register_callback_query_handler(
         commands_callback.send_menu,
         func=lambda c: c.data.startswith('commands')
@@ -71,7 +73,7 @@ def register_callback_query_handlers(bot):
     logger.info('callback_query_handlers registered')
 
 
-def register_all_handlers(bot):
+def register_all_handlers(bot: TeleBot) -> TeleBot:
     logger.info('Starting handlers registration...')
     registrer_admin_msg_handlers(bot)
     register_private_msg_handlers(bot)
